071ddcad3f2459cc399da721525e42d3279434c0
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-05-14 10:14:06 -0400

    Added code and some data to the repository

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 ctrug-tweet
 ===========
+
+This project contains the R sources and some of the data to run the examples I presented at the CT R 
+Users Group meeing on May 15, 2012. The focus of this talk was to show how R can be used to evaluate
+text to generate a "sentiment score". It's significantly inspired by Jeff Breens [tutorial](https://github.com/jeffreybreen/twitter-sentiment-analysis-tutorial-201107) on sentiment scoring of tweets pertaining to airlines.
+
+My example is based on the work of [Prof. Debarchana Ghosh](http://www.geography.uconn.edu/people/ghosh.html)
+ who is investigating health realated issues such as obesity, using Twitter as one possible data source. The 
+current dataset contains approximately 2M tweets related to the topic of pizza being [classified as a 
+vegetable](http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CIMBEBYwAA&url=http%3A%2F%2Fwww.msnbc.msn.com%2Fid%2F45306416%2Fns%2Fhealth-diet_and_nutrition%2Ft%2Fpizza-vegetable-congress-says-yes%2F&ei=zBGxT9btA-Pr0gHCpdykDA&usg=AFQjCNHX7_7RIhSUiFbOH-F15EXeVaK_dg)
+
+Tweets were downloaded over an extended period of time using a PHP client. Sentiment scoring is performed 
+using a simple dictionary lookup. The first approach uses Breens word lists while the second approach
+employs [SentiWordNet](http://sentiwordnet.isti.cnr.it/), which provides a finer way to score sentiments
+(though I don't go into the details of [POS tagging](http://en.wikipedia.org/wiki/Part-of-speech_tagging)
+
+I should point out that I'm not a text-mining expert and am still on the fence on the validity of sentiment
+scoring. This presentation focuses on the use of R in such an application.
---
+++ senti.R
@@ -0,0 +1,67 @@
+library(multicore)
+library(ggplot2)
+library(igraph)
+library(reshape)
+
+d <- read.csv('pizza-unique.csv', colClass='character', comment='', header=TRUE)
+d$geox <- as.numeric(d$geox)
+d$geoy <- as.numeric(d$geoy)
+
+remove.urls <- function(x) gsub("http.*$", "", gsub('http.*\\s', ' ', x))
+remove.html <- function(x) gsub('&quot;', '', x)
+
+d$text <- remove.urls(d$text)
+d$text <- remove.html(d$text)
+d$text <- gsub("@", "FOOBAZ", d$text)
+d$text <- gsub("[[:punct:]]+", " ", d$text)
+d$text <- gsub("FOOBAZ", "@", d$text)
+d$text <- gsub("[[:space:]]+", ' ', d$text)
+d$text <- tolower(d$text)
+
+## Simple approach, based on code/slides from Jeff Breen
+posw <- read.table('breen/positive-words.txt', header=FALSE, comment=';', as.is=TRUE)[,1]
+negw <- read.table('breen/negative-words.txt', header=FALSE, comment=';', as.is=TRUE)[,1]
+
+score.breen <- function(tweet) {
+  words <- strsplit(tweet, "\\s+")[[1]]
+  pm <- sum(!is.na(match(words, posw)))
+  nm <- sum(!is.na(match(words, negw)))
+  return(pm-nm)
+}
+
+## Use SentiWordNet - slightly better resolution
+swn <- read.csv('sentinet_r.csv', header=TRUE, as.is=TRUE)
+## distribution of parts of speech
+tmp <- table(swn$POS)
+names(tmp) <- c('', 'Adjective', 'Noun', 'Adverb', 'Verb')
+tmp <- data.frame(tmp)[-1,]
+ggplot(tmp, aes(x=Var1, y=Freq))+geom_bar(stat='identity')+
+  xlab("Part of Speech") + ylab("Frequency")
+
+## distribution of 'objectivity' of the individual words
+tmp <- swn
+tmp$os <- 1-(tmp$PosScore+tmp$NegScore)
+ggplot(tmp, aes(x=os))+geom_histogram(colour='black', fill='white')+
+  xlab("Objectivity Score") + ylab("Frequency")
+
+
+## match a term to SWN and return the pos & neg scores
+swn.match <- function(w) {
+  tmp <- subset(swn, Term == w)
+  if (nrow(tmp) >= 1)
+    return(tmp[1,c(3,4)])
+  else return(c(0,0))
+}
+
+## generate a 'sentiment score' using SentiWordNet
+score.swn <- function(tweet) {
+  words <- strsplit(tweet, "\\s+")[[1]]
+  cs <- colSums(do.call('rbind',
+                        lapply(words, function(z) swn.match(z))))
+  return(cs[1]-cs[2])
+}
+
+scores.swn <- mclapply(d$text, score.swn)
+scores.breen <- mclapply(d$text, score.breen)
+
+save.image('work.Rda')
