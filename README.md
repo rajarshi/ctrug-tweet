e9fa6309412c1192f074daf959fe1ddae47c3676
Rajarshi  Guha <rajarshi.guha@gmail.com>
2012-05-15 15:13:45 -0400

    Some more visualizations. More or less finished the slides. Added note on data availability

--- README.md
+++ README.md
@@ -13,7 +13,11 @@ vegetable](http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0C
 Tweets were downloaded over an extended period of time using a PHP client. Sentiment scoring is performed 
 using a simple dictionary lookup. The first approach uses Breens word lists while the second approach
 employs [SentiWordNet](http://sentiwordnet.isti.cnr.it/), which provides a finer way to score sentiments
-(though I don't go into the details of [POS tagging](http://en.wikipedia.org/wiki/Part-of-speech_tagging))
+(though I don't go into the details of [POS tagging](http://en.wikipedia.org/wiki/Part-of-speech_tagging)). The
+Breen word lists are available from this repository. However, I cannot redistribute the SentiWord list and
+even though I processed it into a simple CSV for the example code, I don't think I have permission to 
+redistribute it.
 
 I should point out that I'm not a text-mining expert and am still on the fence on the validity of sentiment
-scoring. This presentation focuses on the use of R in such an application.
+scoring. This presentation focuses on the use of R in such an application and not on the pros/cons of 
+sentiment analysis itself.
--- senti.R
+++ senti.R
@@ -31,12 +31,28 @@ score.breen <- function(tweet) {
 
 ## Use SentiWordNet - slightly better resolution
 swn <- read.csv('sentinet_r.csv', header=TRUE, as.is=TRUE)
+swn <- subset(swn, !is.nan(PosScore) & !is.na(ID))
+
 ## distribution of parts of speech
 tmp <- table(swn$POS)
 names(tmp) <- c('', 'Adjective', 'Noun', 'Adverb', 'Verb')
 tmp <- data.frame(tmp)[-1,]
 ggplot(tmp, aes(x=Var1, y=Freq))+geom_bar(stat='identity')+
   xlab("Part of Speech") + ylab("Frequency")
+
+## evaluate whether each word is pos, neg or neutral
+## by taking the value of pos-neg: >0 => pos, <0 => neg etc
+status <- sapply(1:nrow(swn), function(x) {
+  if (swn$Pos[x] > swn$Neg[x]) return("positive")
+  else if (swn$Pos[x] < swn$Neg[x]) return("negative")
+  else return("neutral")
+})
+tmp <- data.frame(POS=swn$POS, Status=status)
+tmp <- data.frame(do.call('rbind', by(tmp, tmp$POS, function(x) table(x$Status))))
+tmp$pos <- c('adjective', 'noun', 'adverb', 'verb')
+tmp <- melt(tmp, id='pos', variable_name = 'Sentiment')
+ggplot(tmp, aes(x=pos,y=value,fill=Sentiment))+geom_bar(position='fill')+
+  xlab("")+ylab("Proportion")
 
 ## distribution of 'objectivity' of the individual words
 tmp <- swn
@@ -70,12 +86,36 @@ score.swn.2 <- function(tweet) {
   return(cs[1]-cs[2])  
 }
 
+## 6052 sec for score.swn
+## 461 sec for score.swn2
+
 #scores.swn <- mclapply(d$text, score.swn)
 scores.swn <- mclapply(d$text, score.swn.2)
 scores.breen <- mclapply(d$text, score.breen)
 
-save.image('work2.Rda')
+save.image('work.Rda')
 
+# lets explore scores
+d$swn <- unlist(scores.swn)
+d$breen <- unlist(scores.breen)
 
-## 6052 sec for score.swn
-## 461 sec for score.swn2
+tmp <- rbind(data.frame(Method='SWN', Scores=d$swn),
+             data.frame(Method='Breen', Scores=d$breen))
+ggplot(tmp, aes(x=Scores, fill=Method))+geom_density(alpha=0.25)+
+  xlab("Sentiment Scores")
+
+# geocoded
+library(maps)
+tmp <- subset(d, !is.na(geox) & geoy < -65 & geoy > -130 & geox > 25 & geox < 50)
+m <- map_data('state')
+ggplot()+
+  geom_polygon( data=m, aes(x=long, y=lat, group=group),
+               colour='grey70', fill=NA) +
+  geom_point(data=tmp, aes(geoy, geox, colour=swn, size=abs(swn)), alpha=0.75) +
+  opts(axis.text.x = theme_blank(),
+       axis.text.y = theme_blank(),
+       axis.ticks = theme_blank(),
+       axis.title.x = theme_blank(),
+       axis.title.y = theme_blank()) +
+  xlab("") + ylab("") +
+  scale_colour_gradient(low="red", high="green")
